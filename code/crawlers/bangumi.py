0045d37f2ea81d67245135e5bde5c6b328edffea
Martin_Z_He <me@apassbydreg.work>
2020-10-29 14:20:16 +0800

    add update modual

--- code/crawlers/bangumi.py
+++ code/crawlers/bangumi.py
@@ -3,6 +3,23 @@ import requests
 import json
 import time
 
+from datetime import datetime
+
+
+def get_index():
+    """
+        获取 index
+        output:
+            - index (dict)
+    """
+    url = "https://cdn.jsdelivr.net/npm/anime-sachedule-search-data@0.1.106/dist/data.json"
+    res = requests.get(url)
+    if res:
+        idx = json.loads(res.text)
+    else:
+        raise ConnectionError("failed to get index from cdn")
+    return idx
+
 
 def is_valid(subject):
     """
@@ -26,7 +43,6 @@ def is_valid(subject):
         return False
 
 
-
 def get_subject(subject_id):
     """
         根据主题 ID 获取 bangumi 信息
@@ -44,7 +60,7 @@ def get_subject(subject_id):
     return data
 
 
-def crawl_all(index, dest='data/bangumi_all.json'):
+def crawl_all(index):
     """
         根据给定的 anime_index 爬取所有来自 bangumi 的信息，并存储到 dest 表示的文件中
         input:
@@ -53,8 +69,8 @@ def crawl_all(index, dest='data/bangumi_all.json'):
         output:
             - records (dict) { subject_id(str) => subject(dict) }
     """
+    # 分析 index
     waitlist = []
-
     for item in index['items']:
         sites = item.get('sites', {})
         bangumi_subject_id = None
@@ -65,6 +81,7 @@ def crawl_all(index, dest='data/bangumi_all.json'):
             waitlist.append(bangumi_subject_id)
     print("loaded {} subjects to waitlist".format(len(waitlist)))
 
+    # 按任务队列爬取信息
     FAILED_THRES = 3
     failed_times = {}
     n_repeat = len(waitlist) * 2
@@ -81,14 +98,35 @@ def crawl_all(index, dest='data/bangumi_all.json'):
             else:
                 status = 'invalid'
         except:
+            # 如果出现问题则在有限失败次数内重新加入队列等待重试
             status = 'failed'
             failed_times[subject_id] = failed_times.get(subject_id, 0) + 1
             if failed_times[subject_id] < FAILED_THRES:
                 time.sleep(1)
                 waitlist.append(subject_id)
         print("try {} {}: {} crawled, {} remaining".format(run_times, status, len(records), len(waitlist)))
-    
-    fp = open(dest, 'w', encoding='utf-8')
-    json.dump(records, fp, ensure_ascii=False)
 
     return records
+
+
+def update_data():
+    """
+        自动更新番剧内容，直接保存到文件（无传入/返回值）
+    """
+    date_str = datetime.today().isoformat()[:10]
+    index_path = "data/bangumi_idx@{}.json".format(date_str)
+    bangumi_all_path = "data/bangumi_all@{}.json".format(date_str)
+
+    idx = get_index()
+    fp = open(index_path, 'w', encoding='utf-8')
+    json.dump(idx, fp, ensure_ascii=False)
+
+    records = crawl_all(idx)
+    fp = open(bangumi_all_path, 'w', encoding='utf-8')
+    json.dump(records, fp, ensure_ascii=False)
+
+    print("data updated @ {}".format(date_str))
+
+
+if __name__ == "__main__":
+    update_data()
