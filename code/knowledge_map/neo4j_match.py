69a7fdc657fc0e2f16d2d1bd05dfd7145ca52f0b
zengbohan0217 <13811753844@126.com>
2020-12-01 14:13:53 +0800

    完善了说明

--- code/knowledge_map/neo4j_match.py
+++ code/knowledge_map/neo4j_match.py
@@ -6,6 +6,7 @@ from py2neo import Node, Relationship, Graph, NodeMatcher, RelationshipMatcher
 def shortest_path_match(graph, name_1, attrib_1, attrib_2):
     """
     用于查找单个关系
+    for example: p = shortest_path_match(graph, "悠木碧", "actor", "charactor")
     """
     match_str = "MATCH (p:" + attrib_1 + \
                 "{name: '" + name_1 + "' })-[r]-(q:" + attrib_2 + ") RETURN q"
@@ -17,11 +18,11 @@ def shortest_path_match(graph, name_1, attrib_1, attrib_2):
 def recommend_path(graph, name_1, attrib_1, attrib_2, attrib_3):
     """
     这部分用于推荐
+    for example: p = recommend_path(graph, "恋如雨止", "comic", "actor", "comic")
     """
     match_str = "MATCH (p:" + attrib_1 + \
                 "{name: '" + name_1 + "' })-[r]-(q:" + attrib_2 + \
-                ")-[t]-(w:" + attrib_3 + ")-[t1]-(w1:" + attrib_2 + \
-                ") RETURN w1"
+                ")-[t]-(w:" + attrib_3 + ") RETURN w"
     print(match_str)
     p1 = graph.run(match_str)
     p1 = list(p1)
