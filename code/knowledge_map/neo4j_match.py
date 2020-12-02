fde85a972fac0afbcf5897c24200c0638cc9e60b
zengbohan0217 <13811753844@126.com>
2020-12-02 16:06:12 +0800

    加了点简单的东西

--- code/knowledge_map/neo4j_match.py
+++ code/knowledge_map/neo4j_match.py
@@ -1,4 +1,7 @@
 from py2neo import Node, Relationship, Graph, NodeMatcher, RelationshipMatcher
+import jieba.posseg as pseg
+import jieba
+import json
 
 #my_password = ''
 #graph = Graph('http://localhost:7474', username='neo4j', password=my_password)
@@ -27,3 +30,38 @@ def recommend_path(graph, name_1, attrib_1, attrib_2, attrib_3):
     p1 = graph.run(match_str)
     p1 = list(p1)
     return p1
+
+def get_comic_list(json_path):
+    """
+    获得全部comic的名字以及其中声优和charactor的名字
+    """
+    with open(json_path, 'r', encoding="UTF-8") as fp:
+        dict = json.load(fp)
+    comic_list = []
+    actor_list = []
+    charactor_list = []
+    for key, value in dict.items():
+        comic_list.append(value["番剧"])
+        for charactor_inform in value["角色"]:
+            if charactor_inform["name"] not in charactor_list:
+                charactor_list.append(charactor_inform["name"])
+            for actor in charactor_inform["actors"]:
+                if actor not in actor_list:
+                    actor_list.append(actor)
+    return comic_list, actor_list, charactor_list
+
+def Q_and_A():
+    special_list = []
+    comic_list, actor_list, charactor_list = get_comic_list("./data/bangumi_simplify.json")
+    special_list.extend(comic_list)
+    special_list.extend(actor_list)
+    special_list.extend(charactor_list)
+    for word in special_list:
+        jieba.suggest_freq(word, True)
+    question = input("年轻的孩子，用你的问题换取宅之力吧！")
+
+    words = pseg.cut(question)
+    for word, flag in words:
+        print(word)
+        print(flag)
+
